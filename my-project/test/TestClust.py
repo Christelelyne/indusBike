f56a8171c15b3c30851dbd67b987467009314c65
Christelelyne <50624959+Christelelyne@users.noreply.github.com>
2019-11-24 22:05:06 +0100

    Add files via upload

--- my-project/test/TestClust.py
+++ my-project/test/TestClust.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 from citybike.CityBike import *
 
 class testClust (unittest.TestCase):
@@ -9,16 +10,19 @@ class testClust (unittest.TestCase):
     """
     
     def test_preprocessing(self):
+        """ Test that the preprocessing works fine
+            It removes all the non relevant values
+        """
     
         mycluster = Clust("Brisbane_CityBike.json")
         res = mycluster.dataPreprocessing()
         self.assertEqual(res.count(), 143)
     
     def test_Kmeans(self):
-        
+        """ Check the average cost of the model"""
         mycluster = Clust("Brisbane_CityBike.json")
-        res = mycluster.Kmeans(mycluster.dataPreprocessing())
-        print (res.count())
+        avgCost = mycluster.Kmeans(mycluster.dataPreprocessing())
+        self.assertEqual(round(avgCost,4),0.005)
         
         
 
